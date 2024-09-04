e9106aa839d21dcacb20249d060c85be3ab2dd64
00ivanfarias <ivanfarias099@outlook.com>
2024-09-04 11:32:33 -0300

    backup
    web v2 load to -> origin:develope

---
+++ README.md
@@ -0,0 +1,1 @@
+# TortuInternet-Website
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'redHat': ['"Red Hat Display"','sans-serif'],
+        'lato': ['"Lato"','sans-serif']
+      },
+      colors:{
+        'accent-green':'#24AB15',
+        'sec-green':'#08520F',
+        'light-green':'#E4F3CE',
+        'negative':'#040404',
+        'positive': '#f3f3f3'
+      }
+    },
+  },
+  plugins: [],
+}

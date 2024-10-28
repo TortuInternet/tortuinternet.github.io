8e42f466dc7f63d3ac42512adc0ec07797db3ca1
Ivan Farias(bl4ckston3) <ivanfarias099@outlook.com>
2024-10-28 11:36:08 -0300

    feat
    faq section build v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./index.html"
-  ],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
-      fontFamily:{
-        'redHat': ['"Red Hat Display"','sans-serif'],
-        'lato': ['"Lato"','sans-serif']
+      fontFamily: {
+        redHat: ['"Red Hat Display"', "sans-serif"],
+        lato: ['"Lato"', "sans-serif"],
       },
-      colors:{
-        'accent-green':'#24AB15',
-        'sec-green':'#08520F',
-        'light-green':'#E4F3CE',
-        'negative':'#040404',
-        'positive': '#f3f3f3'
-      }
+      colors: {
+        "accent-green": "#24AB15",
+        "sec-green": "#08520F",
+        "light-green": "#E4F3CE",
+        negative: "#040404",
+        positive: "#f3f3f3",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
